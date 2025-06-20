f7f49a964b60218139c03d2980e248f95611155e
MahirAbsar <mahirabsar181@gmail.com>
2025-06-20 19:23:50 +0600

    feat: setup and initialize clerk

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,11 @@
-import { style } from "@/styles/auth.styles";
+import { styles } from "@/styles/auth.styles";
 import { Link } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={style.container}>
-      <Text style={style.title}>This is a text component</Text>
+    <View style={styles.container}>
+      <Text style={{ color: "white" }}>This is a text component</Text>
       <Link href="/notifications">Notifications</Link>
       <TouchableOpacity onPress={() => alert("Danger")}>
         <Text>Press Here</Text>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,20 @@
+import { ClerkProvider } from "@clerk/clerk-expo";
 import { Stack } from "expo-router";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
-        <Stack
-          screenOptions={{
-            headerShown: false,
-          }}
-        />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
+          <Stack
+            screenOptions={{
+              headerShown: false,
+            }}
+          />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ClerkProvider>
   );
 }
